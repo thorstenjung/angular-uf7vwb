62cb706cbbb5f59002c8c2096428e927c6d6081a
thorstenjung <mail@thorstenjung.com>
2022-07-07 23:41:07 +0200

    Passing data between components

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent {
+  @Input() productInput: Product | undefined;
+  @Output() notify = new EventEmitter();
+  constructor() {}
+}
