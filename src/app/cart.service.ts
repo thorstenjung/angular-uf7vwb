e277ff3b49a071bebb713f36498e687a9b7155d9
Thorsten Jung <mail@thorstenjung.com>
2022-07-12 18:33:04 +0200

    Going through angular.io tutorial without adding forms and inputs

---
+++ src/app/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  clearItems(){
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: String, price: Number}[]>('/assets/shipping.json');
+  }
+}
